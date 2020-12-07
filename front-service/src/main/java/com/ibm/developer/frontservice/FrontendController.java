f98fa3dbca7d28a869d2914c9c7a6714547981e2
Billy Korando <wkorando@gmail.com>
2020-12-07 16:32:18 -0600

    Adding front service message

--- front-service/src/main/java/com/ibm/developer/frontservice/FrontendController.java
+++ front-service/src/main/java/com/ibm/developer/frontservice/FrontendController.java
@@ -1,5 +1,6 @@
 package com.ibm.developer.frontservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -9,9 +10,16 @@ public class FrontendController {
 
 	private RestTemplate restTemplate = new RestTemplate();
 
+	private String welcomeMessage;
+
+	public FrontendController(@Value("${WELCOME_MESSAGE}") String welcomeMessage) {
+		this.welcomeMessage = welcomeMessage;
+	}
+	
+	
 	@GetMapping
 	public String returnHelloMessage() throws InterruptedException {
 		String podName = restTemplate.getForObject("http://backend-service:8080", String.class);
-		return "Hello, " + podName;
+		return welcomeMessage + " " + podName;
 	}
 }
